c48eb3d467fe37b6d72478864cf67f29cba20621
Szymon Suszko <szymonsuszko@gmail.com>
2021-02-04 09:20:11 +0100

    final touch

--- apps/api/src/app/bookmark.resolver.ts
+++ apps/api/src/app/bookmark.resolver.ts
@@ -14,10 +14,22 @@ export class BookmarkResolver {
       id: 1,
       name: 'Wirtualna Polska',
       url: 'https://www.wp.pl/',
-      groupId: 7
+      groupId: 1
     },
     {
       id: 2,
+      name: 'Guardian',
+      url: 'https://www.theguardian.com',
+      groupId: 2
+    },
+    {
+      id: 3,
+      name: 'Onet',
+      url: 'https://www.onet.pl/',
+      groupId: 2
+    },
+    {
+      id: 4,
       name: 'Kwejk',
       url: 'https://kwejk.pl/',
       groupId: 1
